449b77e0c73ac63ff059c2e130a22313b4423a7a
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-19 23:38:57 +0300

    console uı test

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Entities.Concrete;
+
+BookWriter bookWriter = new() {Id=1, Name="furkan",SurName="Bozkurt"};
+GraphicDirector GraphicDirector = new() { Id=2,Name=bookWriter.Name,SurName=bookWriter.SurName};
+
+Console.WriteLine(bookWriter.Name+" "+bookWriter.SurName);
+Console.WriteLine(GraphicDirector.Name+" "+GraphicDirector.SurName);
