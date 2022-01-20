0b9833a8aee902124f854297783a71b6936c7c52
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-20 18:54:04 +0300

    connection builder db

---
+++ DataAccess/Concrete/LRMSPostgreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class LRMSPostgreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseNpgsql("Host = my_host; Database=my_db;Username=my_user;Password=my_pw");
+
+
+
+    }
+}
