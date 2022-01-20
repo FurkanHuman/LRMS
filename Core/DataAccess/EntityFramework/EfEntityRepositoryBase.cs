2eb54468722f9d0fdc55196297cae68c05de113e
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-20 18:53:07 +0300

    added db repository base

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess.PostgreDb
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public TEntity? Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using TContext context = new();
+            return context.Set<TEntity>().SingleOrDefault(filter.Compile());
+        }
+
+        public IList<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+        {
+            using TContext context = new();
+            return filter == null ?
+            context.Set<TEntity>().ToList() :
+            context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Add(TEntity entity)
+        {
+            using TContext context = new();
+            EntityEntry<TEntity> AddEntity = context.Entry(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using TContext context = new();
+            EntityEntry<TEntity> deleteEntity = context.Remove(entity);
+        }
+
+
+        public void Update(TEntity entity)
+        {
+            using TContext context = new();
+            EntityEntry<TEntity> UpdateEntity = context.Entry(entity);
+            UpdateEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        T? Get(Expression<Func<T, bool>> filter);
+        IList<T> GetAll(Expression<Func<T, bool>>? filter = null);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
