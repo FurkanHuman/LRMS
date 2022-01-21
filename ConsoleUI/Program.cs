4b6ab01fc6aeff6536d8b3d3c41752f57f2db710
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-22 01:02:10 +0300

    Clear

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,4 @@
-﻿using Entities.Concrete.BookFirstPage;
+﻿
 
-BookWriter bookWriter = new() { Id = 1, Name = "furkan", SurName = "Bozkurt" };
-GraphicDesignOrDirector graphicDesignOrDirector = new() { Id = 2, Name = bookWriter.Name, SurName = bookWriter.SurName };
 
-Console.WriteLine(bookWriter.Name + " " + bookWriter.SurName);
-Console.WriteLine(graphicDesignOrDirector.Name + " " + graphicDesignOrDirector.SurName);
+
