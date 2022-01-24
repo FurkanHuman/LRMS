503c2cb75157f37fef124c4284974c25e611b41e
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-24 04:10:20 +0300

    Added new dbset

--- DataAccess/Context/PostgreDbContext.cs
+++ DataAccess/Context/PostgreDbContext.cs
@@ -1,13 +1,13 @@
-﻿using Entities.Concrete.BookCover;
-using Entities.Concrete.BookFirstPage;
+﻿using Entities.Concrete;
+using Entities.Concrete.Cover;
+using Entities.Concrete.FirstPage;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Context
 {
     public class PostgreDbContext : DbContext
     {
-        public PostgreDbContext():base()
+        public PostgreDbContext() : base()
         {
 
         }
@@ -16,19 +16,26 @@ namespace DataAccess.Context
         {
             optionsBuilder.UseNpgsql("Host=localhost,Database=LRMS_DataBase;Username=postgres;Password=12345");
         }
-        // Book Frontend
-        public DbSet<BookCoverCap> BookCovers { get; set; }
-        public DbSet<BookCategory> BookCategories { get; set; }
-        public DbSet<BookCoverImage> BookCoverImages { get; set; }
+        // entity Frontend
+        public DbSet<CoverCap> BookCovers { get; set; }
+        public DbSet<Category> BookCategories { get; set; }
+        public DbSet<CoverImage> BookCoverImages { get; set; }
 
-        // Book Backend
-        public DbSet<BookEditor> BookEditors { get; set; }
-        public DbSet<BookTechnicalNumber> BookTechnicalNumbers { get; set; }
-        public DbSet<BookWriter> BookWriters { get; set; }
+        // entity Backend
+        public DbSet<Editor> BookEditors { get; set; }
+        public DbSet<TechnicalNumber> BookTechnicalNumbers { get; set; }
+        public DbSet<Writer> BookWriters { get; set; }
         public DbSet<Edition> Editions { get; set; }
         public DbSet<GraphicDesignOrDirector> GraphicDesignOrDirectors { get; set; }
         public DbSet<Interpreters> Interpreters { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
         public DbSet<Redaction> Redactions { get; set; }
+
+        // entity full
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Encyclopedia> Encyclopedias { get; set; }
+        public DbSet<Magazine> Magazines { get; set; }
+        public DbSet<NewsPaper> NewsPapers { get; set; }
+
     }
 }
