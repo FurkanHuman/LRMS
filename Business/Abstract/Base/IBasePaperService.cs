e8be1235ce462bc747864b36d7e672f03ccc8317
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-26 00:53:08 +0300

    Base interface generate of inherite inteface. code cleanup

---
+++ Business/Abstract/Base/IBasePaperService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Result.Abstract;
+using Entities.Concrete.Base;
+
+namespace Business.Abstract.Base
+{
+    public interface IBasePaperService<T> where T : BasePaper
+    {
+        IDataResult<T> GetById(int id);
+        IDataResult<List<T>> GetByName(string names);
+        IDataResult<List<T>> GetList();
+        IDataResult<List<T>> GetByCategory(int categoryId);
+        IDataResult<List<T>> GetByWriter(int writerId);
+        IDataResult<List<T>> GetByEdition(int editionNum);
+        IDataResult<List<T>> GetByPublisher(int publisherNum);
+        IDataResult<List<T>> GetByCoverCap(int CoverCapNum);
+        IDataResult<List<T>> GetByFilterList();
+        IResult Add(T Entity);
+        IResult Delete(T Entity);
+        IResult Update(T Entity);
+    }
+}
---
+++ Business/Abstract/Base/IFirstPersonBaseService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Result.Abstract;
+using Entities.Concrete.Base;
+
+namespace Business.Abstract
+{
+    public interface IFirstPersonBaseService<T> where T : FirstPagePersonBase
+    {
+        IDataResult<T> GetById(int id);
+        IDataResult<T> GetByName(string name);
+        IDataResult<T> GetBySurname(string surname);
+        IDataResult<List<T>> GetList();
+        IDataResult<List<T>> GetByFilterList();
+        IResult Add(T Entity);
+        IResult Delete(T Entity);
+        IResult Update(T Entity);
+    }
+}
