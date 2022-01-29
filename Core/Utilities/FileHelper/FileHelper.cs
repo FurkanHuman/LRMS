3768a2716b0ea8ae7221db2d3250f2839f4620cc
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-01-29 04:13:53 +0300

    filehelper written for file operations

---
+++ Core/Utilities/FileHelper/FileHelper.cs
@@ -0,0 +1,57 @@
+﻿using Core.Utilities.Result.Abstract;
+using Core.Utilities.Result.Concrete;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Utilities.FileHelper
+{
+    public class FileHelper:IFileHelper
+    {
+        public string FullPath { get; set; }
+
+        public IDataResult<string> AddAsync(IFormFile file)
+        {
+            var (path, halfPath) = NewPath(file);
+
+            Task.Run(() =>
+            {
+                using FileStream filestream = File.Create(path);
+                file.CopyTo(filestream);
+                filestream.Flush();
+            });
+            return new SuccessDataResult<string>(halfPath);
+        }
+
+        public IDataResult<string> UpdateAsync(string sourcePath, IFormFile file)
+        {
+            var (path, halfPath) = NewPath(file);
+            Task.Run(() =>
+            {
+                using FileStream stream = File.Create(path);
+                file.CopyTo(stream);
+                stream.Flush();
+                DeleteAsync(sourcePath);
+            });
+            return new SuccessDataResult<string>(halfPath);
+        }
+
+        public IResult DeleteAsync(string path)
+        {
+            Task.Run(() => File.Delete(Environment.CurrentDirectory + path));
+            return new SuccessResult();
+        }
+
+        private (string path, string halfPath) NewPath(IFormFile file)
+        {
+            string fileExtension = Path.GetExtension(file.FileName);
+
+            string creatingUniqueFilename = Guid.NewGuid().ToString("B") + fileExtension;
+
+            string result = FullPath + creatingUniqueFilename;
+
+            if (!Directory.Exists(FullPath))
+                Directory.CreateDirectory(FullPath);
+
+            return (result, @"\Images\" + creatingUniqueFilename);
+        }
+    }
+}
---
+++ Core/Utilities/FileHelper/IFileHelper.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Result.Abstract;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.FileHelper
+{
+    public interface IFileHelper
+    {
+        public string FullPath { get; set; }
+        IDataResult<string> AddAsync(IFormFile file);
+        IDataResult<string> UpdateAsync(string sourcePath, IFormFile file);
+        IResult DeleteAsync(string path);
+    }
+}
