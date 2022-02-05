6d25493f56a5fab3f24f427147e765398a69fb17
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-02-06 01:23:13 +0300

    TechnicalPlaceholder dal layers added

---
+++ DataAccess/Abstract/ITechnicalPlaceholderDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using Entities.Concrete.Infos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ITechnicalPlaceholderDal: IEntityRepository<TechnicalPlaceholder>
+    {
+    }
+}
