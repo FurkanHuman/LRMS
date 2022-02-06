74d196e717054df7865cfeefb80281fd582ee14d
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-02-06 20:11:30 +0300

    add new entitys

---
+++ Entities/Concrete/Base/MaterialBase.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Abstract;
+using Entities.Concrete.Infos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.Base
+{
+    public class MaterialBase
+    {
+        [Key, JsonIgnore]
+        public int Id { get; set; }
+
+        [Required, MaxLength(512)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(512)]
+        public string Description { get; set; }
+
+        [Required]
+        public List<Category> Categories { get; set; }
+
+        [Required]
+        public TechnicalPlaceholder TechnicalPlaceholder { get; set; }
+
+    }
+}
---
+++ Entities/Concrete/BookSeries.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class BookSeries : Book, IEntity
+    {
+        public int BookNumber { get; set; }
+        
+        public List<Book> Books { get; set; }
+    }
+}
