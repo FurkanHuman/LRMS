3e489ad5ee9b491dbaaab722809135bb458dfa0f
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-02-08 18:19:27 +0300

    new field entering

--- Entities/Concrete/Base/MaterialBase.cs
+++ Entities/Concrete/Base/MaterialBase.cs
@@ -13,19 +13,24 @@ namespace Entities.Concrete.Base
     public class MaterialBase
     {
         [Key, JsonIgnore]
-        public int Id { get; set; }
+        public ulong Id { get; set; }
 
         [Required, MaxLength(512)]
         public string Name { get; set; }
 
         [Required, MaxLength(512)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(512)]
         public string Description { get; set; }
+
+        [Required, MaxLength(512)]
+        public string State { get; set; }
 
         [Required]
         public List<Category> Categories { get; set; }
 
         [Required]
-        public TechnicalPlaceholder TechnicalPlaceholder { get; set; }
-
+        public List<TechnicalPlaceholder> TechnicalPlaceholders { get; set; }
     }
 }
