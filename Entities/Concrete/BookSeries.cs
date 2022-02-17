44b7b71184f52ba12fafe0f5601d8ce569dddda8
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-02-17 17:57:03 +0300

    change class

--- Entities/Concrete/BookSeries.cs
+++ Entities/Concrete/BookSeries.cs
@@ -1,16 +1,10 @@
 ﻿using Core.Entities.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Entities.Concrete.Base;
 
 namespace Entities.Concrete
 {
-    public class BookSeries : Book, IEntity
+    public class BookSeries : BasePaper, IEntity
     {
-        public int BookNumber { get; set; }
-        
         public List<Book> Books { get; set; }
     }
 }
