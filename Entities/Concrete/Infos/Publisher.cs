0ca2d9f9382e91649f9337e86b6b217d8b86a33d
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-11 02:21:52 +0300

    Write if you understand

--- Entities/Concrete/Infos/Publisher.cs
+++ Entities/Concrete/Infos/Publisher.cs
@@ -17,14 +17,13 @@ namespace Entities.Concrete.Infos
         public string Address { get; set; }
 
         [Required]
-        public ulong PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
 
-        public ulong? FaxNumber { get; set; }
+        public string? FaxNumber { get; set; }
+
+        public string? WebSite { get; set; }
 
         public DateTime DateOfPublication { get; set; }
-
-        [Required]
-        public string WebSite { get; set; }
 
         public bool IsDeleted { get; set; }
     }
