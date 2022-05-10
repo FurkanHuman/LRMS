c959cbe0104b3078a54ef82b2d5d0b76355ee014
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-11 02:16:17 +0300

    added country field.

---
+++ Entities/Concrete/Infos/Country.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Entities.Concrete.Infos
+{
+    public class Country : IEntity
+    {
+        [Key, JsonIgnore]
+        public ulong Id { get; set; }
+
+        [Required]
+        public string CountryName { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
