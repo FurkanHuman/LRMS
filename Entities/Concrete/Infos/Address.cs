ee1b819561574f00a61889bc985398764f00ecd8
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-11 02:10:51 +0300

    address entity added.

---
+++ Entities/Concrete/Infos/Address.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Entities.Concrete.Infos
+{
+    public class Address: IEntity
+    {
+        [Key, JsonIgnore]
+        public ulong Id { get; set; }
+       
+        [Required]
+        public Country Country { get; set; }
+
+        [Required]
+        public City City { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+        
+        [Required]
+        public string AddressLine1 { get; set; }
+
+        [Required]
+        public string AddressLine2 { get; set; }
+
+        public string? GeoLocation { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
