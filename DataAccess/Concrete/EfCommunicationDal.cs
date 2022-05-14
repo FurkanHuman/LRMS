6173da04d9085bd0af7affa4e93b624501fd4b38
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-14 19:33:11 +0300

    Communication Entity Added

---
+++ DataAccess/Concrete/EfCommunicationDal.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess.PostgreDb;
+using DataAccess.Abstract;
+using DataAccess.Context;
+using Entities.Concrete.Infos;
+
+namespace DataAccess.Concrete
+{
+    public class EfCommunicationDal : EfEntityRepositoryBase<Communication, PostgreDbContext>, ICommunicationDal
+    {
+    }
+}
---
+++ Entities/Concrete/Infos/Communication.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities.Abstract;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Entities.Concrete.Infos
+{
+    public class Communication : IEntity
+    {
+        [Key, JsonIgnore]
+        public int Id { get; set; }
+
+        [Required]
+        public string CommunicationName { get; set; }
+
+        [Required]
+        public Address Address { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        public string? FaxNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string WebSite { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
