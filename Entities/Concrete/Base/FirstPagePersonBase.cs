0bf09913ade01260af254e20119ada12766bdae4
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-15 23:40:34 +0300

    Big operation Guid Change

--- Entities/Concrete/Base/FirstPagePersonBase.cs
+++ Entities/Concrete/Base/FirstPagePersonBase.cs
@@ -6,7 +6,7 @@ namespace Entities.Concrete.Base
     public class FirstPagePersonBase
     {
         [Key, JsonIgnore]
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
         public string Name { get; set; }
 
