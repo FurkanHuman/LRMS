b900945edbbdc454484efbf070e7b6d8a195f3b2
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-18 04:37:02 +0300

    Basic json reader

---
+++ Core/Utilities/JsonHelper/Abstract/IJsonReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.JsonHelper.Abstract
+{
+    public interface IJsonReader
+    {
+        string Reader(string? jsonPath, string jsonFile, string jsonKey);
+        string Reader(string jsonFile, string jsonKey);
+    }
+}
---
+++ Core/Utilities/JsonHelper/Concrete/JsonReaderMicrosoft.cs
@@ -0,0 +1,37 @@
+﻿using Core.Utilities.JsonHelper.Abstract;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Utilities.JsonHelper.Concrete
+{
+    public class JsonReaderMicrosoft : IJsonReader
+    {
+
+        public string Reader(string? jsonPath, string jsonFile, string jsonKey)
+        {
+            IConfigurationBuilder configuration = new ConfigurationBuilder();
+            configuration.SetBasePath(jsonPath);
+
+            if (jsonPath == null)
+                configuration.SetBasePath(Directory.GetCurrentDirectory());
+
+            configuration.AddJsonFile(jsonFile);
+
+            IConfigurationRoot config = configuration.Build();
+
+            string connectionString = config[jsonKey];
+            return connectionString;
+        }
+        public string Reader(string jsonFile, string jsonKey)
+        {
+            IConfigurationBuilder configuration = new ConfigurationBuilder();
+
+            configuration.SetBasePath(Directory.GetCurrentDirectory());
+            configuration.AddJsonFile(jsonFile);
+
+            IConfigurationRoot config = configuration.Build();
+
+            string connectionString = config[jsonKey];
+            return connectionString;
+        }
+    }
+}
