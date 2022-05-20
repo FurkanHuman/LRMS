6ee0f977bc1148a1f4abdd22b84c0996a13ecefe
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-21 00:45:35 +0300

    experimental codes trying

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,16 @@
-﻿
+﻿using Business.Concrete;
 using Core.Utilities.JsonHelper.Abstract;
 using Core.Utilities.JsonHelper.Concrete;
-using Microsoft.EntityFrameworkCore;
+using DataAccess.Concrete;
+using DataAccess.Context;
+using Entities.Concrete.Infos;
+using Npgsql;
+using System.Data;
+
+
+
+BranchManager branchManager=new (new EfBranchDal());
+
+branchManager.Add("Graphic Designer");
 
 Console.ReadLine();
