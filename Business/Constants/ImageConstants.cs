fc598c0f82dd3b8b317c21214bbbe2cb81c42185
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-21 01:02:18 +0300

    file ext. fix, move to efmaterialfile

--- Business/Constants/ImageConstants.cs
+++ Business/Constants/ImageConstants.cs
@@ -4,11 +4,10 @@ namespace Business.Constants
 {
     public class ImageConstants : BaseConstants
     {
-        public static readonly string[] ImageExtension = { ".jpeg", ".jpg", ".png", ".gif", ".pdf", ".bmp", ".docx", ".doc", ".tiff" };
+        public static readonly string[] ImageExtension = { ".jpeg", ".jpg", ".png", ".gif", ".bmp",".tiff" };
         public const string DataStatusUnchanged = "Data status unchanged.";
         public const string DataStatusChanged = "Data status Changed.";
         public const string FileDeleted = "File Deleted.";
         public const string IsDeleted = "File Not Found.";
-        public const string InvalidFileSize = "Invalid file size.";
     }
 }
