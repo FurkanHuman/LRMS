6b80b44ff8bc4fda798fe3f27ab07178bf2718d9
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-21 01:04:33 +0300

    Hımm🤔🤔🤔

---
+++ Business/ValidationRules/FluentValidation/ImageValidator.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete.Infos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ImageValidator : AbstractValidator<Image>
+    {
+        public ImageValidator()
+        {
+        }
+    }
+}
