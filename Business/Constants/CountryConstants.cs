17db07e1c568db47b7a91ddd0e145da285da0db4
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-22 22:19:32 +0300

    added message

--- Business/Constants/CountryConstants.cs
+++ Business/Constants/CountryConstants.cs
@@ -9,5 +9,6 @@ namespace Business.Constants
         public const string CountryCodeNull = "Country code is null.";
         public const string CountryCodeLong = "Country code too long.";
         public const string CountryNameAndCodeNotMatch = "Country Name And Code do not match.";
+        public const string CountryNotFound= "Country not found.";
     }
 }
