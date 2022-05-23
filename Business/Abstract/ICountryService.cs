295138df4306703b97e23e373b151cf0c533622b
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-23 18:54:48 +0300

    added new method, fix method params and auto fix

--- Business/Abstract/ICountryService.cs
+++ Business/Abstract/ICountryService.cs
@@ -6,12 +6,10 @@ namespace Business.Abstract
     public interface ICountryService
     {
         IResult Add(Country country);
-        IResult AddCities(int countryId, int[] cityIds);
         IResult Delete(int conutryId);
         IResult ShadowDelete(int conutryId);
-        IResult Update(int oldCountryId, Country newCountry);
+        IResult Update(Country country);
         IDataResult<Country> GetByCountry(int countryId);
-        IDataResult<List<City>> GetByCities(int countryId);
         IDataResult<List<Country>> GetAll();
     }
 }
