100291905ff5ca1f4932edfe83143aa488023077
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-28 01:56:38 +0300

    structure change. new event added and more

--- Business/Abstract/IAddressService.cs
+++ Business/Abstract/IAddressService.cs
@@ -9,6 +9,7 @@ namespace Business.Abstract
         IResult Delete(Guid addressGId);
         IResult ShadowDelete(Guid addressGId);
         IResult Update(Address address);
+        IDataResult<Address> GetById(Guid guid);
         IDataResult<List<Address>> GetByPostalCode(string postalCode);
         IDataResult<List<Address>> GetByCityId(int cityId);
         IDataResult<List<Address>> GetByCountryId(int countryId);
