d3359cda0a97f970c2bf79927040527dce3916ef
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-27 15:52:46 +0300

    reference update. test test test 🤔🤔🤔

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello World!");
+﻿string originalStr = "dog running walk way with owners";
+
+bool find = originalStr.Contains("walK",StringComparison.CurrentCultureIgnoreCase);
+
+if (find)
+    Console.WriteLine(find);
