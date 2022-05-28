92387590b15218539607b18667ad7ee00db6d98d
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-28 18:38:24 +0300

    c.cp

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,6 @@
 ﻿string originalStr = "dog running walk way with owners";
 
-bool find = originalStr.Contains("walK",StringComparison.CurrentCultureIgnoreCase);
+bool find = originalStr.Contains("walK", StringComparison.CurrentCultureIgnoreCase);
 
 if (find)
     Console.WriteLine(find);
