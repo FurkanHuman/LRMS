8c3b9522d86f2a98694bd55c895795dc96aac7bf
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-05-31 16:15:49 +0300

    "Name" and "IsDeleted" field added.

--- Entities/Concrete/Infos/Dimension.cs
+++ Entities/Concrete/Infos/Dimension.cs
@@ -9,10 +9,15 @@ namespace Entities.Concrete.Infos
         [Key, JsonIgnore]
         public Guid Id { get; set; }
 
+        [Required]
+        public string Name { get; set; }
+
         public double Width { get; set; }
 
         public double Height { get; set; }
 
         public double Length { get; set; }
+
+        public bool IsDeleted { get; set; }
     }
 }
