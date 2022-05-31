48a732779947f2a934396db1d1cc08ca414ad8f9
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-01 01:02:36 +0300

    unused "errored" roles removed.

--- Business/ValidationRules/FluentValidation/CommunicationValidator.cs
+++ Business/ValidationRules/FluentValidation/CommunicationValidator.cs
@@ -10,11 +10,9 @@ namespace Business.ValidationRules.FluentValidation
         {
             RuleFor(c => c.CommunicationName).MinimumLength(5).MaximumLength(50)
                 .NotNull().NotEmpty().WithMessage(CommunicationConstants.CommNameError);
-            RuleFor(c => c.Address).NotNull().NotEmpty().WithMessage(CommunicationConstants.AddressNull);
             RuleFor(c => c.PhoneNumber).NotNull().NotEmpty().WithMessage(CommunicationConstants.PhoneNull);
             RuleFor(c => c.Email).NotNull().NotEmpty().WithMessage(CommunicationConstants.EmailNull);
             RuleFor(c => c.WebSite).NotNull().NotEmpty().WithMessage(CommunicationConstants.WebSiteNull);
-            RuleFor(c => c.Address.Id).NotNull().NotEmpty().WithMessage(CommunicationConstants.AddressNull);
         }
     }
 }
