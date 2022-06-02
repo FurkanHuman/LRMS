913a4c06301b961a77ed9649f888f6d7e9a1a734
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-02 13:43:38 +0300

    add city controller

---
+++ LRMS/Controllers/CitiesController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Entities.Concrete.Infos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LRMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private ICityService _cityService;
+
+        public CitiesController(ICityService cityService)
+        {
+            _cityService = cityService;
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(City city)
+        {
+            var result = _cityService.Add(city);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = _cityService.Delete(id);  
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update(City city)
+        {
+            var result = _cityService.Update(city);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _cityService.GetAll();
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
