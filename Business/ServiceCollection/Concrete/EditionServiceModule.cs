8365055e93bda609d61924a3f6b88b23747a6da3
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-09 15:40:35 +0300

    Solution attempt for constructor hell.

---
+++ Business/ServiceCollection/Concrete/EditionServiceModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using Business.ServiceCollection.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ServiceCollection.Concrete
+{
+    public class EditionServiceModule : Module, IEditionServiceCollection
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AddressManager>().As<IAddressService>().SingleInstance();
+            builder.RegisterType<CityManager>().As<ICityService>().SingleInstance();
+            builder.RegisterType<CommunicationManager>().As<ICommunicationService>().SingleInstance();
+            builder.RegisterType<CountryManager>().As<ICountryService>().SingleInstance();
+        }
+    }
+}
