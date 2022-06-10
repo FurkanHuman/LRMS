5f5539b08acfbba55a24f1740d975ac2c59cdfb1
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-10 17:50:29 +0300

    c.cp

--- Business/ServiceCollection/Concrete/EditionServiceModule.cs
+++ Business/ServiceCollection/Concrete/EditionServiceModule.cs
@@ -2,11 +2,6 @@
 using Business.Abstract;
 using Business.Concrete;
 using Business.ServiceCollection.Abstract;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Business.ServiceCollection.Concrete
 {
