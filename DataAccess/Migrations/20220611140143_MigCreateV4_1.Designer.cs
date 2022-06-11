08e1eabc6249663fe8b051c3e0183609b97dd209
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-11 17:05:51 +0300

    category, kit relationship fixed

--- DataAccess/Migrations/20220611140143_MigCreateV4_1.Designer.cs
+++ DataAccess/Migrations/20220611140143_MigCreateV4_1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DataAccess.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(PostgreDbContext))]
-    partial class PostgreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220611140143_MigCreateV4_1")]
+    partial class MigCreateV4_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
