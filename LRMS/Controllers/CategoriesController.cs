23cd7c7c29719014be2091a1ed51dc3cda2a77ca
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-11 20:35:42 +0300

    test successful. controller successful.

---
+++ LRMS/Controllers/CategoriesController.cs
@@ -0,0 +1,93 @@
+﻿using Business.Abstract;
+using Entities.Concrete.Infos;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace LRMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(string categoryName)
+        {
+            Category category = new() { CategoryName = categoryName };
+            var result = _categoryService.Add(category);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var result = _categoryService.Delete(id);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("ShadowDelete")]
+        public IActionResult ShadowDelete(int id)
+        {
+            var result = _categoryService.ShadowDelete(id);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update(int id, string categoryName)
+        {
+            Category category = new() { CategoryName = categoryName };
+            var result = _categoryService.Update(category);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("GetByNames")]
+        public IActionResult GetByNames(string name)
+        {
+            var result = _categoryService.GetByNames(name);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost("GetByFilterLists")]
+        public IActionResult GetByFilterLists(Expression<Func<Category, bool>>? filter = null)
+        {
+            var result = _categoryService.GetByFilterLists(filter);
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetAllBySecrets")]
+        public IActionResult GetAllBySecrets()
+        {
+            var result = _categoryService.GetAllBySecrets();
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
---
+++ LRMS_Test/Tests/CategoryTest.cs
@@ -0,0 +1,97 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete.Infos;
+
+namespace LRMS_Test.Tests
+{
+    // [TestClass]
+    public class CategoryTest
+    {
+        private readonly ICategoryService _categoryService = new CategoryManager(new EfCategoryDal());
+
+        [TestMethod]
+        public void AddTest()
+        {
+            Category category = new() { CategoryName = "Astronomi" };
+
+            var testAdd = _categoryService.Add(category);
+
+            Assert.IsTrue(!testAdd.Success, "Add Test Success");
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            int id = 8;
+
+            var testDelete = _categoryService.Delete(id);
+
+            Assert.IsTrue(!testDelete.Success, "Delete Test Succsess");
+        }
+
+        [TestMethod]
+        public void ShadowDeleteTest()
+        {
+            int id = 10;
+
+            var testShadowDelete = _categoryService.ShadowDelete(id);
+
+            Assert.IsFalse(testShadowDelete.Success, "Shadow Delete Test Succsess");
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Category category = new() { CategoryName = "Harita", Id = 9 };
+
+            var testUpdate = _categoryService.Update(category);
+
+            Assert.IsFalse(!testUpdate.Success, "Update Test Success");
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            int id = 7;
+
+            var testGetById = _categoryService.GetById(id);
+
+            Assert.IsTrue(testGetById.Success, "Get By Id Test Success");
+        }
+
+        [TestMethod]
+        public void GetByName()
+        {
+            var testGetByName = _categoryService.GetByNames("Kor");
+
+            Assert.IsTrue(testGetByName.Success, " Get By Names Test Success");
+        }
+
+        [TestMethod]
+        public void GetByFilterLists()
+        {
+            int id = 10;
+
+            var testGetByFilterLists = _categoryService.GetByFilterLists(b => b.Id == id);
+
+            Assert.IsTrue(testGetByFilterLists.Success, "Get By Filter Lists Test Success");
+        }
+
+        [TestMethod]
+        public void GetAllBySecrets()
+        {
+            var testGetAllBySecrets = _categoryService.GetAllBySecrets();
+
+            Assert.IsTrue(testGetAllBySecrets.Success, "Get All By Secrets Test Success");
+        }
+
+        [TestMethod]
+        public void Getall()
+        {
+            var testGetAll = _categoryService.GetAll();
+
+            Assert.IsTrue(testGetAll.Success, "Get All Test Success");
+        }
+    }
+}
