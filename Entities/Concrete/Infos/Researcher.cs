bb80195f4817783b986d67f1477e1bb348d717a9
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-12 04:18:52 +0300

    [Required] attributed added

--- Entities/Concrete/Infos/Researcher.cs
+++ Entities/Concrete/Infos/Researcher.cs
@@ -6,12 +6,14 @@ namespace Entities.Concrete.Infos
 {
     public class Researcher : FirstPagePersonBase, IEntity
     {
-        public string? NamePreAttachment { get; set; }
+        [Required]
+        public string NamePreAttachment { get; set; }
 
         [Required]
         public string Specialty { get; set; }
 
-        public University? University { get; set; }
+        [Required]
+        public University University { get; set; }
 
         public IList<AcademicJournal> AcademicJournals { get; set; }
         public IList<Dissertation> Dissertations { get; set; }
