e583138847b2ee07601eb5456ce4ee9b2602e89a
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-12 04:19:16 +0300

    nullable added

--- Entities/Concrete/Infos/Writer.cs
+++ Entities/Concrete/Infos/Writer.cs
@@ -6,7 +6,7 @@ namespace Entities.Concrete.Infos
 
     public class Writer : FirstPagePersonBase, IEntity
     {
-        public string NamePreAttachment { get; set; }
+        public string? NamePreAttachment { get; set; }
 
         public IList<Book> Books { get; set; }
         public IList<BookSeries> BookSeries { get; set; }
