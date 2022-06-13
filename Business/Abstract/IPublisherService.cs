7367c15bc0a74b050fba3d2846263af5ad4f41b5
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-13 16:05:10 +0300

    name fix, sorthing

--- Business/Abstract/IPublisherService.cs
+++ Business/Abstract/IPublisherService.cs
@@ -6,22 +6,22 @@ namespace Business.Abstract
 {
     public interface IPublisherService : IBaseEntityService<Publisher, Guid>
     {
-        IDataResult<Publisher> GetByAdderssId(Guid addressId);
-        IDataResult<List<Publisher>> GetByPublisherInCountryId(int countryId);
+        IDataResult<Publisher> GetByAddressId(Guid addressId);
+        IDataResult<Publisher> GetByCommunicationId(Guid commId);
+        IDataResult<Publisher> GetByCommunicationEmail(string commEmail);
+        IDataResult<Publisher> GetByCommunicationFaxNumber(string commFaxNumber);
+        IDataResult<Publisher> GetByCommunicationPhone(string commPhone);
+        IDataResult<Publisher> GetByCommunicationWebSite(string commWebSite);
         IDataResult<List<Publisher>> GetByAddressName(string addressName);
         IDataResult<List<Publisher>> GetByAddressLines(string addressLine);
-        IDataResult<List<Publisher>> GetByPublisherInCountryName(string countryName);
-        IDataResult<List<Publisher>> GetByPublisherInCountryCode(string countryCode);
         IDataResult<List<Publisher>> GetByPublisherInCityId(int cityId);
         IDataResult<List<Publisher>> GetByPublisherInCityName(string cityName);
-        IDataResult<List<Publisher>> GetByPublisherInPostalCode(string postalCode);
-        IDataResult<List<Publisher>> GetByPublisherInGeoLocation(string geoLoc);
-        IDataResult<Publisher> GetByCommunicationId(Guid commId);
         IDataResult<List<Publisher>> GetByCommunicationName(string commName);
-        IDataResult<Publisher> GetByCommunicationPhone(string commPhone);
-        IDataResult<Publisher> GetByCommunicationFaxNumber(string commFaxNumber);
-        IDataResult<Publisher> GetByCommunicationEmail(string commEmail);
-        IDataResult<Publisher> GetByCommunicationWebSite(string commWebSite);
+        IDataResult<List<Publisher>> GetByPublisherInCountryId(int countryId);
+        IDataResult<List<Publisher>> GetByPublisherInCountryName(string countryName);
+        IDataResult<List<Publisher>> GetByPublisherInCountryCode(string countryCode);
+        IDataResult<List<Publisher>> GetByPublisherInGeoLocation(string geoLoc);
+        IDataResult<List<Publisher>> GetByPublisherInPostalCode(string postalCode);
         IDataResult<List<Publisher>> GetByDateOfPublication(DateTime dateOfPublication);
         IDataResult<List<Publisher>> GetByDateOfPublicationMinMax(DateTime minDate, DateTime maxDate);
     }
--- Business/Abstract/IUniversityService.cs
+++ Business/Abstract/IUniversityService.cs
@@ -7,12 +7,12 @@ namespace Business.Abstract
     public interface IUniversityService : IBaseEntityService<University, Guid>
     {
         IDataResult<University> GetByAddressId(Guid id);
-        IDataResult<List<University>> GetByInstituteNames(string instituteName);
-        IDataResult<List<University>> GetByBranchNames(string branchName);
         IDataResult<List<University>> GetByBranchId(int branchId);
-        IDataResult<List<University>> GetByCityNames(string cityName);
+        IDataResult<List<University>> GetByBranchNames(string branchName);
         IDataResult<List<University>> GetByCityId(int cityId);
-        IDataResult<List<University>> GetByCountryNames(string countryName);
+        IDataResult<List<University>> GetByCityNames(string cityName);
         IDataResult<List<University>> GetByCountryId(int countryId);
+        IDataResult<List<University>> GetByCountryNames(string countryName);
+        IDataResult<List<University>> GetByInstituteNames(string instituteName);
     }
 }
