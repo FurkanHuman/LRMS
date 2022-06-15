a21d7ff28b2838cc75dde6d91475f2bc59880da1
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-15 03:55:41 +0300

    will be continued

--- Business/Abstract/IAcademicJournalService.cs
+++ Business/Abstract/IAcademicJournalService.cs
@@ -1,9 +1,15 @@
 ﻿using Business.Abstract.Base;
+using Core.Utilities.Result.Abstract;
 using Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface IAcademicJournalService : IMaterialBaseService<AcademicJournal>
     {
+        IDataResult<List<AcademicJournal>> GetByResearcher(Guid researcherId);
+        IDataResult<List<AcademicJournal>> GetByResearcherLists(Guid[] researcherIds);
+
+        IDataResult<List<AcademicJournal>> GetByEditor(Guid id);
+        IDataResult<List<AcademicJournal>> GetByEditorLists(Guid[] id);
     }
 }
