2649a6511279609724e636d8901ef9b8b8a893e4
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-16 19:53:15 +0300

    new messages added

--- Business/Constants/Base/BaseConstants.cs
+++ Business/Constants/Base/BaseConstants.cs
@@ -35,5 +35,23 @@
         public const string Maintenance = "In maintenance phase.";
         public const string Test = "Testing, there may be an incorrect result";
         public const string BuildedTime = "this medhod builded a time ago, but not was write a code.";
+        public const string NameRequired = "Name is required";
+        public const string FpNameLengthRangeMinMax = "Name length must be between {0} and {1} characters.";
+        public const string SurnameRequired = "Surname is required";
+        public const string FpSurnameLengthRangeMinMax = "Surname length must be between {0} and {1} characters.";
+        public const string NameLengthRangeMinMax = "Name length must be between 3 and 56";
+        public const string TitleRequired = "Title is required";
+        public const string TitleLengthRangeMinMax = "Title length must be between 3 and 75";
+        public const string DescriptionRequired = "Description is required";
+        public const string DescriptionLengthRangeMinMax = "Description length must be between 3 and 500";
+        public const string CategoryIdRequired = "Category is required";
+        public const string TechnicalPlaceholdersIdRequired = "TechnicalPlaceholders is required";
+        public const string StateRequired = "State is required";
+        public const string CoverCapIdRequired = "CoverCapId is required";
+        public const string CoverImageIdRequired = "CoverImageId is required";
+        public const string WriterIdRequired = "WriterId is required";
+        public const string EditorIdRequired = "EditorId is required";
+        public const string TechnicalNumberIdRequired = "TechnicalNumberId is required";
+        public const string EditionIdRequired = "EditionId is required";
     }
 }
