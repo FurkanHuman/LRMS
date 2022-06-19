dfa2643ea907d5f6c9c703aa3d16ecca1caefa7f
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-20 01:48:09 +0300

    parameter name change

--- Business/Abstract/Base/IBasePaperService.cs
+++ Business/Abstract/Base/IBasePaperService.cs
@@ -7,15 +7,15 @@ namespace Business.Abstract.Base
     public interface IBasePaperService<T> : IMaterialBaseService<T> where T : BasePaper, IEntity, new()
     {
         IDataResult<T> GetByCoverImage(Guid cImageId);
-        IDataResult<List<T>> GetByCoverCaps(byte CoverCapNum);
+        IDataResult<List<T>> GetByCoverCaps(byte coverCapNum);
         IDataResult<List<T>> GetByCommunications(Guid communicationId);
         IDataResult<List<T>> GetByDirectors(Guid directorId);
         IDataResult<List<T>> GetByEditors(Guid editorId);
-        IDataResult<List<T>> GetByEditions(Guid editionNum);
+        IDataResult<List<T>> GetByEditions(Guid editionId);
         IDataResult<List<T>> GetByGraphicDirectors(Guid graphicDirectorId);
         IDataResult<List<T>> GetByGraphicDesigns(Guid graphicDesignId);
         IDataResult<List<T>> GetByInterpreters(Guid interpreterId);
-        IDataResult<List<T>> GetByPublishers(Guid publisherNum);
+        IDataResult<List<T>> GetByPublishers(Guid publisherId);
         IDataResult<List<T>> GetByTechnicalNumbers(Guid technicalNumberId);
         IDataResult<List<T>> GetByRedactions(Guid redactionId);
         IDataResult<List<T>> GetByWriters(Guid writerId);
