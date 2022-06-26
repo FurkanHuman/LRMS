fa5e6da1d08cf3d088e9a382cfa5812e7744b35e
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-06-26 22:36:45 +0300

    city fixed by address

--- Entities/Concrete/Infos/City.cs
+++ Entities/Concrete/Infos/City.cs
@@ -10,10 +10,8 @@ namespace Entities.Concrete.Infos
 
         public string CityName { get; set; }
 
-        public bool IsDeleted { get; set; }
-
         public int CountryId { get; set; }
 
-        public Country Country { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
