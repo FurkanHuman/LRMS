892b1fd57af5b26598d07130bc0d30d56f0192b2
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-20 02:11:21 +0300

    validator write finished

---
+++ Business/ValidationRules/FluentValidation/MagazineValidator.cs
@@ -0,0 +1,15 @@
+﻿using Business.Constants;
+using Business.ValidationRules.FluentValidation.Base;
+using Entities.Concrete;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class MagazineValidator : BasePaperValidator<Magazine>
+    {
+        public MagazineValidator()
+        {
+            RuleFor(m => m.MagazineType).NotEmpty().NotNull().WithMessage(MagazineConstants.TypeNull);
+            RuleFor(m => m.Volume).NotEmpty().NotNull().WithMessage(MagazineConstants.VolumeNull);
+        }
+    }
+}
