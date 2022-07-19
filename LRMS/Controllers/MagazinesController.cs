affbc1473ac78fff0feed21bfe0213e60a90b5ad
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-20 02:12:16 +0300

    ctor injected service

---
+++ LRMS/Controllers/MagazinesController.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LRMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MagazinesController : ControllerBase
+    {
+        private readonly IMagazineService _magazineService;
+
+        public MagazinesController(IMagazineService magazineService)
+        {
+            _magazineService = magazineService;
+        }
+    }
+}
