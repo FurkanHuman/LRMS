6d44cc01d53eae0872e9b5b3901e559d40d50630
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-20 02:08:04 +0300

    magazine constants file added & write a message

---
+++ Business/Constants/MagazineConstants.cs
@@ -0,0 +1,24 @@
+﻿using Business.Constants.Base;
+
+namespace Business.Constants
+{
+    public class MagazineConstants : BaseConstants
+    {
+        protected MagazineConstants()
+        {
+
+        }
+
+        public enum MagazineTypes // todo research magazine type , current example
+        {
+            Other,
+            Kids,
+            bla,
+            blaa,
+            Blaaa
+        }
+
+        public const string TypeNull = "Magazine Type is null.";
+        public const string VolumeNull = "Magazine volume is null.";
+    }
+}
