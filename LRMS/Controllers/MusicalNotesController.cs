053732db5a39e7b5a60fdc596389773412887e32
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-28 02:10:57 +0300

    implemented service.

---
+++ LRMS/Controllers/MusicalNotesController.cs
@@ -0,0 +1,18 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LRMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicalNotesController : ControllerBase
+    {
+        private readonly IMusicalNoteService _musicalNoteService;
+
+        public MusicalNotesController(IMusicalNoteService musicalNoteService)
+        {
+            _musicalNoteService = musicalNoteService;
+        }
+    }
+}
