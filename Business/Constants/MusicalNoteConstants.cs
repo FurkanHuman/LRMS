13b561bf343f3faed919832d990d7914b9b764a6
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-28 02:09:53 +0300

    new messages added

---
+++ Business/Constants/MusicalNoteConstants.cs
@@ -0,0 +1,12 @@
+﻿using Business.Constants.Base;
+
+namespace Business.Constants
+{
+    public class MusicalNoteConstants : BaseConstants
+    {
+        protected MusicalNoteConstants() { }
+
+        public const string ComposerNull = "Composer is Null";
+        public const string DateNull = "Writing Date is Null";
+    }
+}
