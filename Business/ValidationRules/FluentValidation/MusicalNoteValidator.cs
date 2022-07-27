d008a629d6066eb78621d8ec4f57fe6770a35225
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-28 02:10:32 +0300

    validator completed

---
+++ Business/ValidationRules/FluentValidation/MusicalNoteValidator.cs
@@ -0,0 +1,15 @@
+﻿using Business.Constants;
+using Business.ValidationRules.FluentValidation.Base;
+using Entities.Concrete;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class MusicalNoteValidator : MaterialBaseValidator<MusicalNote>
+    {
+        public MusicalNoteValidator()
+        {
+            RuleFor(mn => mn.ComposerId).NotNull().NotEmpty().WithMessage(MusicalNoteConstants.ComposerNull);
+            RuleFor(mn => mn.DateOfWriting).NotNull().NotEmpty().WithMessage(MusicalNoteConstants.DateNull);
+        }
+    }
+}
