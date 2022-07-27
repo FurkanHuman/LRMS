5c0afb762b6e3c3bdeb6aebecd3f88e794df7de6
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-28 02:06:35 +0300

    new services added.

--- Business/Abstract/IMusicalNoteService.cs
+++ Business/Abstract/IMusicalNoteService.cs
@@ -1,9 +1,13 @@
 ﻿using Business.Abstract.Base;
+using Core.Utilities.Result.Abstract;
 using Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface IMusicalNoteService : IMaterialBaseService<MusicalNote>
     {
+        IDataResult<List<MusicalNote>> GetAllByComposer(Guid composerId);
+        IDataResult<List<MusicalNote>> GetAllByComposers(Guid[] composerIds);
+        IDataResult<List<MusicalNote>> GetAllByDateOfWriting(DateTime dateOfWriting);
     }
 }
