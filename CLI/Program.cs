ab10230ef5421c8af45d0e96359155bab3ee8ddc
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-29 17:57:31 +0300

    test update

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities.Concrete.Infos;
+
 Console.WriteLine("Hello, World!");
+
+ICategoryDal _CategoryDal = new EfCategoryDal();
+
+IEnumerable<Category> categories = _CategoryDal.IGetAll();
+
+foreach (var item in categories)
+{
+    Console.WriteLine(item.Name);
+}
--- LRMS_Test/Tests/CountryTest.cs
+++ LRMS_Test/Tests/CountryTest.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LRMS_Test.Tests
 {
     [TestClass]
     public class CountryTest
     {
-        [TestMethod]
-        public void MyTestMethod()
-        {
+        ICountryDal _CountryDal = new EfCountryDal();
 
+        [TestMethod]
+        public void GetAll()
+        {
+            var country = _CountryDal.GetAll();
+
+            Assert.IsTrue(true, "Test Ok");
         }
 
+        [TestMethod]
+        public void IGetAll()
+        {
+            var country = _CountryDal.IGetAll();
+
+            Assert.IsTrue(true, "Test Ok");
+        }
     }
 }
