4814fb6177433adbf38d85eb0d2f4816416e06de
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-07-31 23:48:40 +0300

    cleaned

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,15 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Business.DependencyResolvers;
 using DataAccess.Abstract;
 using DataAccess.Concrete;
 using Entities.Concrete.Infos;
 
 Console.WriteLine("Hello, World!");
-
-ICategoryDal _CategoryDal = new EfCategoryDal();
-
-IEnumerable<Category> categories = _CategoryDal.IGetAll();
-
-foreach (var item in categories)
-{
-    Console.WriteLine(item.Name);
-}
