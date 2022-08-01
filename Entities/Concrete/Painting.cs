9a6056324bb9c06df4f632727b418fd9b4982ebd
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-01 03:33:58 +0300

    change types

--- Entities/Concrete/Painting.cs
+++ Entities/Concrete/Painting.cs
@@ -8,14 +8,14 @@ namespace Entities.Concrete
     public class Painting : MaterialBase, IEntity
     {
         [Required]
-        public string Owner { get; set; }
+        public OtherPeople Owner { get; set; }
 
         [Required]
         public Guid ImageId { get; set; }
 
         public bool IsDestroyed { get; set; }
 
-        public Image Image { get; set; }
+        public IList<Image> Image { get; set; }
 
         public IList<Kit> Kits { get; set; }
     }
