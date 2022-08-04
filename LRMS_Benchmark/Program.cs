8e7b3f3c255d2328b953a792d17a4c1dbc3eb476
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-05 01:18:40 +0300

    update

--- LRMS_Benchmark/Program.cs
+++ LRMS_Benchmark/Program.cs
@@ -1,25 +1,16 @@
-﻿
-using Autofac;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
-using Business.Concrete;
-using Business.DependencyResolvers.Facade;
-using Core.Utilities.Result.Abstract;
-using DataAccess.Concrete;
-using Entities.Concrete.Infos;
-using Microsoft.Extensions.Hosting;
 
 namespace LRMS_Benchmark
 {
     public class Program
     {
-        private readonly EfWriterDal _efCategoryDal = new();
-
+        
         [Benchmark]
-        public void IGetAllBechMark() => _efCategoryDal.IGetAll();
-
-        [Benchmark]
-        public void GetAllBechMark() => _efCategoryDal.GetAll();
+        public void GetAllBechMark()
+        {
+            
+        } 
 
 
         static void Main(string[] args)
