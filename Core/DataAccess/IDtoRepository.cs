b04a215c4bafa695cdeb3b1557236f9b08e88554
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-06 17:15:12 +0300

    Dto repository created

---
+++ Core/DataAccess/IDtoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.Abstract;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess
+{
+    public interface IDtoRepository<D, T> where D : class, IDto, new()
+                                          where T : class, IEntity, new()
+    {
+        D? DtoGet(Expression<Func<T, bool>> filter);
+        IList<D> DtoGetAll(Expression<Func<T, bool>>? filter = null);
+    }
+}
