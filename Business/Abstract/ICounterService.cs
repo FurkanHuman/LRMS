4bc27088006ce6ae6cef2acc988da118648ef74a
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-09 02:22:17 +0300

    nullable added

--- Business/Abstract/ICounterService.cs
+++ Business/Abstract/ICounterService.cs
@@ -7,7 +7,7 @@ namespace Business.Abstract
 {
     public interface ICounterService : IBaseEntityService<Counter, Guid>
     {
-        Task Count<T>(T t) where T : MaterialBase, IEntity, new();
-        Task Count<T>(IList<T> ts) where T : MaterialBase, IEntity, new();
+        Task Count<T>(T? t) where T : MaterialBase, IEntity, new();
+        Task Count<T>(IList<T>? ts) where T : MaterialBase, IEntity, new();
     }
 }
