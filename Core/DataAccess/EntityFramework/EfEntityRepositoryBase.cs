f8ca528c48536b42fa6b65e80d4c91a2c559a6c6
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-09 02:20:34 +0300

    overload class added && TDto Generic type added

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -6,8 +6,8 @@ using System.Linq.Expressions;
 namespace Core.DataAccess.PostgreDb
 {
     public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
-        where TEntity : class, IEntity, new()
-        where TContext : DbContext, new()
+           where TEntity : class, IEntity, new()
+           where TContext : DbContext, new()
     {
         public TEntity? Get(Expression<Func<TEntity, bool>> filter)
         {
@@ -21,6 +21,61 @@ namespace Core.DataAccess.PostgreDb
             return filter == null ?
                 context.Set<TEntity>().ToList() :
                 context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public void Add(TEntity entity)
+        {
+            using TContext context = new();
+            EntityEntry<TEntity> AddEntity = context.Entry(entity);
+            AddEntity.State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using TContext context = new();
+            EntityEntry<TEntity> deleteEntity = context.Remove(entity);
+            deleteEntity.State = EntityState.Deleted;
+            context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            using TContext context = new();
+            EntityEntry<TEntity> UpdateEntity = context.Entry(entity);
+            UpdateEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+    }
+
+    public class EfEntityRepositoryBase<TEntity, TDto, TContext> : IEntityRepository<TEntity>, IDtoRepository<TDto, TEntity>
+           where TEntity : class, IEntity, new()
+           where TDto : class, IDto, new()
+           where TContext : DbContext, new()
+    {
+        public TEntity? Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using TContext context = new();
+            return context.Set<TEntity>().FirstOrDefault(filter.Compile());
+        }
+
+        public TDto? DtoGet(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+        {
+            using TContext context = new();
+            return filter == null ?
+                context.Set<TEntity>().ToList() :
+                context.Set<TEntity>().Where(filter).ToList();
+        }
+
+        public IList<TDto> DtoGetAll(Expression<Func<TEntity, bool>>? filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public void Add(TEntity entity)
