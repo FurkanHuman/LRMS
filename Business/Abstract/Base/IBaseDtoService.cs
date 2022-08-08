9a86f949545493e1d6b8cc18069bacc4618f4b67
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-09 02:16:49 +0300

    Dto Name changes

--- Business/Abstract/Base/IBaseDtoService.cs
+++ Business/Abstract/Base/IBaseDtoService.cs
@@ -6,8 +6,8 @@ namespace Business.Abstract.Base
     public interface IBaseDtoService<D, I> where D : IDto, new()
                                            where I : struct
     {
-        IResult Add(D entity);
-        IResult Update(D entity);
+        IResult DtoAdd(D entity);
+        IResult DtoUpdate(D entity);
         IDataResult<D> DtoGetById(I id);
         IDataResult<IList<D>> DtoGetAllByIds(I[] ids);
         IDataResult<IList<D>> DtoGetAllByName(string name);
