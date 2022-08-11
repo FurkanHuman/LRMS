fc08620b6ddd14e198f5dcffad28434498b31903
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-11 03:40:50 +0300

    AutoMapper integrated. implemented interface and write. class overloaded write a version seperated Idtos.

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,4 +1,5 @@
-﻿using Core.Entities.Abstract;
+﻿using AutoMapper;
+using Core.Entities.Abstract;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Linq.Expressions;
@@ -49,57 +50,46 @@ namespace Core.DataAccess.PostgreDb
 
     }
 
-    public class EfEntityRepositoryBase<TEntity, TDto, TContext> : IEntityRepository<TEntity>, IDtoRepository<TDto, TEntity>
+    public class EfEntityRepositoryBase<TEntity, TDto, TContext> : IDtoRepository<TEntity>, IDtoRepository<TDto, TEntity>
            where TEntity : class, IEntity, new()
            where TDto : class, IDto, new()
            where TContext : DbContext, new()
     {
-        public TEntity? Get(Expression<Func<TEntity, bool>> filter)
+
+        private readonly IMapper _mapper;
+
+        public EfEntityRepositoryBase()
         {
-            using TContext context = new();
-            return context.Set<TEntity>().FirstOrDefault(filter.Compile());
+            MapperConfiguration config = new(cfg => cfg.CreateMap<TEntity, TDto>());
+            _mapper = config.CreateMapper();
         }
 
         public TDto? DtoGet(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using TContext context = new();
+            return _mapper.Map<TDto>(context.Set<TEntity>().FirstOrDefault(filter.Compile()));
         }
 
-        public IList<TEntity> GetAll(Expression<Func<TEntity, bool>>? filter = null)
+        public C? CDtoGet<C>(Expression<Func<TEntity, bool>> filter ) where C : class, IDto, new()
+        {
+            using TContext context = new();
+            return _mapper.Map<C>(context.Set<TEntity>().FirstOrDefault(filter.Compile()));
+        }
+
+        public IList<C> CDtoGetAll<C>(Expression<Func<TEntity, bool>>? filter = null) where C : class, IDto, new()
         {
             using TContext context = new();
             return filter == null ?
-                context.Set<TEntity>().ToList() :
-                context.Set<TEntity>().Where(filter).ToList();
+               _mapper.Map<IList<C>>(context.Set<TEntity>().ToList()) :
+               _mapper.Map<IList<C>>(context.Set<TEntity>().Where(filter).ToList());
         }
 
         public IList<TDto> DtoGetAll(Expression<Func<TEntity, bool>>? filter = null)
         {
-            throw new NotImplementedException();
-        }
-
-        public void Add(TEntity entity)
-        {
             using TContext context = new();
-            EntityEntry<TEntity> AddEntity = context.Entry(entity);
-            AddEntity.State = EntityState.Added;
-            context.SaveChanges();
-        }
-
-        public void Delete(TEntity entity)
-        {
-            using TContext context = new();
-            EntityEntry<TEntity> deleteEntity = context.Remove(entity);
-            deleteEntity.State = EntityState.Deleted;
-            context.SaveChanges();
-        }
-
-        public void Update(TEntity entity)
-        {
-            using TContext context = new();
-            EntityEntry<TEntity> UpdateEntity = context.Entry(entity);
-            UpdateEntity.State = EntityState.Modified;
-            context.SaveChanges();
+            return filter == null ?
+               _mapper.Map<IList<TDto>>(context.Set<TEntity>().ToList()) :
+               _mapper.Map<IList<TDto>>(context.Set<TEntity>().Where(filter).ToList());
         }
     }
 }
