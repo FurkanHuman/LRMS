811e7aed8788575d2cadf7f4e3e53dde2f2fb9e1
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-11 04:52:06 +0300

    refactoring code

--- DataAccess/Concrete/EfWriterDal.cs
+++ DataAccess/Concrete/EfWriterDal.cs
@@ -1,4 +1,5 @@
-﻿using Core.DataAccess.PostgreDb;
+﻿using AutoMapper;
+using Core.DataAccess.PostgreDb;
 using DataAccess.Abstract;
 using DataAccess.Contexts;
 using Entities.Concrete.Infos;
@@ -7,42 +8,7 @@ using System.Linq.Expressions;
 
 namespace DataAccess.Concrete
 {
-    public class EfWriterDal : EfEntityRepositoryBase<Writer, PostgreDbContext>, IWriterDal
+    public class EfWriterDal : EfEntityRepositoryBase<Writer, WriterDto, PostgreDbContext>, IWriterDal
     {
-        public WriterDto? DtoGet(Expression<Func<Writer, bool>> filter)
-        {
-            using PostgreDbContext context = new();
-            Writer? w = context.Set<Writer>().FirstOrDefault(filter.Compile());
-            return w != null
-                ? new WriterDto
-                {
-                    Id = w.Id,
-                    Name = w.Name,
-                    SurName = w.SurName,
-                    NamePreAttachment = w.NamePreAttachment
-                }
-                : null;
-        }
-
-        public IList<WriterDto> DtoGetAll(Expression<Func<Writer, bool>>? filter = null)
-        {
-            using PostgreDbContext context = new();
-
-            IQueryable<WriterDto> wDto =
-
-                from dw in filter is null
-                      ? context.Writers
-                      : context.Writers.Where(filter)
-
-                select new WriterDto
-                {
-                    Id = dw.Id,
-                    Name = dw.Name,
-                    SurName = dw.SurName,
-                    NamePreAttachment = dw.NamePreAttachment
-                };
-
-            return wDto.ToList();
-        }
     }
 }
