39f52e39ac5d026159f3c05e74afdd32897e0cc1
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-11 03:34:54 +0300

    new generic type version added. new custom methots added.

--- Core/DataAccess/IDtoRepository.cs
+++ Core/DataAccess/IDtoRepository.cs
@@ -3,10 +3,20 @@ using System.Linq.Expressions;
 
 namespace Core.DataAccess
 {
-    public interface IDtoRepository<D, T> where D : class, IDto, new()
-                                          where T : class, IEntity, new()
+    public interface IDtoRepository<D, E> where D : class, IDto, new()
+                                          where E : class, IEntity, new()
     {
-        D? DtoGet(Expression<Func<T, bool>> filter);
-        IList<D> DtoGetAll(Expression<Func<T, bool>>? filter = null);
+        D? DtoGet(Expression<Func<E, bool>> filter);
+
+        IList<D> DtoGetAll(Expression<Func<E, bool>>? filter = null);
+
+    }
+    public interface IDtoRepository<E> where E : class, IEntity, new()
+    {
+        C? CDtoGet<C>(Expression<Func<E, bool>> filter) where C : class, IDto, new();
+
+        // custom IDto type. C is special. if more dto is needed it can be used with the help of methot
+
+        IList<C> CDtoGetAll<C>(Expression<Func<E, bool>>? filter = null) where C : class, IDto, new();
     }
 }
