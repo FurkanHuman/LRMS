3c6bf2e31d2fbd14d7ae1a72ccb089b0a45797a9
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-16 22:58:23 +0300

    Added return type

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -7,8 +7,8 @@ namespace Core.DataAccess
     {
         T? Get(Expression<Func<T, bool>> filter);
         IList<T> GetAll(Expression<Func<T, bool>>? filter = null);
-        void Add(T entity);
-        void Delete(T entity);
-        void Update(T entity);
+        T Add(T entity);
+        T Delete(T entity);
+        T Update(T entity);
     }
 }
