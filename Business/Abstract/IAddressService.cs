46479b6924fc075c8b368ec7a3256f179a1a6d20
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-20 04:29:00 +0300

    added IEntity reference adn new dto methtods

--- Business/Abstract/IAddressService.cs
+++ Business/Abstract/IAddressService.cs
@@ -1,12 +1,16 @@
 ﻿namespace Business.Abstract
 {
-    public interface IAddressService : IBaseEntityService<Address, Guid>
+    public interface IAddressService : IBaseEntityService<Address, Guid>, IBaseDtoService<Address, AddressDto, Guid>
     {
-        IDataResult<IList<AddressDto>> GetAlladdressDtos();
         IDataResult<IList<Address>> GetAllByPostalCode(string postalCode);
+        IDataResult<IList<AddressDto>> DtoGetAllByPostalCode(string postalCode);
         IDataResult<IList<Address>> GetAllByCityId(int cityId);
+        IDataResult<IList<AddressDto>> DtoGetAllByCityId(int cityId);
         IDataResult<IList<Address>> GetAllByCountryId(int countryId);
+        IDataResult<IList<AddressDto>> DtoGetAllByCountryId(int countryId);
         IDataResult<IList<Address>> GetAllByGeoLocation(string geoLocation);
+        IDataResult<IList<AddressDto>> DtoGetAllByGeoLocation(string geoLocation);
         IDataResult<IList<Address>> GetAllBySearchString(string searchStr);
+        IDataResult<IList<AddressDto>> DtoGetAllBySearchString(string searchStr);
     }
 }
--- Business/Abstract/ICountryService.cs
+++ Business/Abstract/ICountryService.cs
@@ -1,7 +1,8 @@
 ﻿namespace Business.Abstract
 {
-    public interface ICountryService : IBaseEntityService<Country, int>
+    public interface ICountryService : IBaseEntityService<Country, int>, IBaseDtoService<Country, CountryDto, int>
     {
         IDataResult<IList<Country>> GetAllByCountryCode(string countryCode);
+        IDataResult<IList<CountryDto>> DtoGetAllByCountryCode(string countryCode);
     }
 }
