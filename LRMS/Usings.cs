7dcfd4327c9796866272b339401a85084e51572f
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-24 02:14:24 +0300

    using update

--- LRMS/Usings.cs
+++ LRMS/Usings.cs
@@ -1,6 +1,7 @@
 ﻿global using Business.Abstract;
 global using Entities.Concrete.DTOs.Gets.Infos;
 global using Entities.Concrete.DTOs.Posts.Adds;
+global using Entities.Concrete.DTOs.Posts.Updates;
 global using Entities.Concrete.Entities.Infos;
 global using Entities.Concrete.Entities.Mains;
 global using System.Linq.Expressions;
