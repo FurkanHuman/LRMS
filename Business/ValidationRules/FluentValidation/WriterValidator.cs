eac5bc2693a321c40a127a19f3bc573c610ec0da
Furkan Bozkurt <furkan-b136@hotmail.com>
2022-08-23 04:37:33 +0300

    implementation change applied

--- Business/ValidationRules/FluentValidation/WriterValidator.cs
+++ Business/ValidationRules/FluentValidation/WriterValidator.cs
@@ -1,12 +1,9 @@
 ﻿namespace Business.ValidationRules.FluentValidation
 {
-    public class WriterValidator : AbstractValidator<Writer>
+    public class WriterValidator : FirstPagePersonBaseValidator<Writer>
     {
         public WriterValidator()
         {
-            RuleFor(w => w.Name).NotEmpty().NotNull().WithMessage(WriterConstants.WriterNameNull);
-            RuleFor(w => w.Name).MinimumLength(3).WithMessage(WriterConstants.WriterNameLengthNotEnough);
-            RuleFor(w => w.SurName).NotEmpty().NotNull().WithMessage(WriterConstants.WriterSurnameNull);
         }
     }
 }
