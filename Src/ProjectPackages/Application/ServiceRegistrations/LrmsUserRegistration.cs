fe8748c0d52a1f7887d8c8a9574167a2467655a8
Furkan Bozkurt <furkan-b136@hotmail.com>
2023-01-27 03:08:55 +0300

    refactor(Application): folder has been moved to the Application root directory.

---
+++ Src/ProjectPackages/Application/ServiceRegistrations/LrmsUserRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Auths.Rules;
+using Application.Services.AuthService;
+using Application.Services.OperationClaimService;
+using Application.Services.PasswordService;
+using Application.Services.RefreshTokenService;
+using Application.Services.UserOperationClaimService;
+using Application.Services.UserService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Services.ServiceRegistrations;
+
+public static class LrmsUserRegistration
+{
+    public static IServiceCollection AddLRMSUserRegistration(this IServiceCollection services)
+    {
+        // LRMS User 
+
+        services.AddScoped<IAuthService, AuthManager>();
+        services.AddScoped<AuthBusinessRules>();
+        
+        services.AddScoped<IPasswordService, PasswordManager>();
+        services.AddScoped<IOperationClaimService, OperationClaimManager>();
+        services.AddScoped<IPasswordService, PasswordManager>();
+        services.AddScoped<IRefreshTokenService, RefreshTokenManager>();
+        services.AddScoped<IUserOperationClaimService, UserOperationClaimManager>();
+        services.AddScoped<IUserService, UserManager>();
+
+        return services;
+    }
+}
