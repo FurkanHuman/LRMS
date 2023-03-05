54b6fefb39da15a3767d5a7d63c19441d1d32b5f
Furkan Bozkurt <furkan-b136@hotmail.com>
2023-03-05 22:24:20 +0300

    feat(Application): Service registrations added for core layers.

---
+++ Src/ProjectPackages/Application/ServiceRegistrations/CoreServiceRegistration.cs
@@ -0,0 +1,25 @@
+﻿using Core.CrossCuttingConcerns.Logging.Serilog.Logger;
+using Core.CrossCuttingConcerns.Logging.Serilog;
+using Core.ElasticSearch;
+using Core.Mailing.MailKitImplementations;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Mailing;
+
+namespace Application.ServiceRegistrations;
+
+public static class CoreServiceRegistration
+{
+    public static IServiceCollection AddCoreServices( this IServiceCollection services)
+    {
+        services.AddSingleton<IMailService, MailKitMailService>();
+        services.AddSingleton<LoggerServiceBase, FileLogger>();
+        services.AddSingleton<IElasticSearch, ElasticSearchManager>();
+
+        return services;
+    }
+}
