e2cc645ebb17ea238e502aa601417e212b6fe770
Furkan Bozkurt <furkan-b136@hotmail.com>
2023-03-05 22:27:20 +0300

    feat(Application): Added core service registration.

--- Src/ProjectPackages/Application/ApplicationServiceRegistration.cs
+++ Src/ProjectPackages/Application/ApplicationServiceRegistration.cs
@@ -1,12 +1,16 @@
-﻿using Application.Services.ServiceRegistrations;
+﻿using Application.ServiceRegistrations;
+using Application.Services.ServiceRegistrations;
 using Core.Application.Pipelines.Authorization;
 using Core.Application.Pipelines.Caching;
 using Core.Application.Pipelines.Logging;
 using Core.Application.Pipelines.Transaction;
 using Core.Application.Pipelines.Validation;
+using Core.Mailing;
+using Core.Mailing.MailKitImplementations;
 using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Reflection;
 
 namespace Application;
@@ -14,11 +18,11 @@ namespace Application;
 public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
-    {
+    {        
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddMediatR(Assembly.GetExecutingAssembly());
-
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
         //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
         //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
         //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>));
@@ -26,6 +30,7 @@ public static class ApplicationServiceRegistration
         //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
         //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionScopeBehavior<,>));
 
+        services.AddCoreServices();
         services.AddLrmsInfoRegistration();
         services.AddLrmsIntermediateTablesRegistration();
         services.AddLRMSMainRegistration();
