8dbf7fe9acd55c2878709df26750c7e3ddb0de4f
Furkan Bozkurt <furkan-b136@hotmail.com>
2023-03-05 22:19:45 +0300

    refactor(Application): class name change & added missing registrations

--- Src/ProjectPackages/Application/ServiceRegistrations/LrmsAuthRegistration.cs
+++ Src/ProjectPackages/Application/ServiceRegistrations/LrmsAuthRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.Auths.Rules;
 using Application.Services.AuthService;
+using Application.Services.EmailAuthenticatorService;
 using Application.Services.OperationClaimService;
+using Application.Services.OtpAuthenticatorService;
 using Application.Services.PasswordService;
 using Application.Services.RefreshTokenService;
 using Application.Services.UserOperationClaimService;
@@ -9,17 +11,18 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.Services.ServiceRegistrations;
 
-public static class LrmsUserRegistration
+public static class LrmsAuthRegistration
 {
     public static IServiceCollection AddLRMSUserRegistration(this IServiceCollection services)
     {
-        // LRMS User 
-
-        services.AddScoped<IAuthService, AuthManager>();
-        services.AddScoped<AuthBusinessRules>();
+        // LRMS User & Auth
         
+        services.AddScoped<AuthBusinessRules>();
+        services.AddScoped<IAuthService, AuthManager>();
         services.AddScoped<IPasswordService, PasswordManager>();
         services.AddScoped<IOperationClaimService, OperationClaimManager>();
+        services.AddScoped<IOtpAuthenticatorService, OtpAuthenticatorManager>();
+        services.AddScoped<IEmailAuthenticatorService, EmailAuthenticatorManager>();
         services.AddScoped<IPasswordService, PasswordManager>();
         services.AddScoped<IRefreshTokenService, RefreshTokenManager>();
         services.AddScoped<IUserOperationClaimService, UserOperationClaimManager>();
