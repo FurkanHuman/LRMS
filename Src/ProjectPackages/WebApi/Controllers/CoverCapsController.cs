be8dba6fa871012b7cef655fb506458ec10c9902
Furkan Bozkurt <furkan-b136@hotmail.com>
2023-03-21 23:16:45 +0300

    feat(WebApi): CoverCap controller filled in.

--- Src/ProjectPackages/WebApi/Controllers/CoverCapsController.cs
+++ Src/ProjectPackages/WebApi/Controllers/CoverCapsController.cs
@@ -1,4 +1,11 @@
 // this file was created automatically.
+using Application.Features.CoverCaps.Commands.CreateCoverCap;
+using Application.Features.CoverCaps.Commands.DeleteCoverCap;
+using Application.Features.CoverCaps.Commands.UpdateCoverCap;
+using Application.Features.CoverCaps.Queries.GetByIdCoverCap;
+using Application.Features.CoverCaps.Queries.GetListByCoverCap;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,5 +21,41 @@ public class CoverCapsController : ControllerBase
     public CoverCapsController(IMediator mediator)
     {
         _mediator = mediator;
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdCoverCapQuery getByIdCoverCapQuery)
+    {
+        GetByIdCoverCapResponse result = await _mediator.Send(getByIdCoverCapQuery);
+        return Ok(result);
+    }
+
+    [HttpGet("GetList")]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListByCoverCapQuery getListByCoverCapQuery = new() { PageRequest = pageRequest };
+        GetListResponse<GetListByCoverCapResponse> result = await _mediator.Send(getListByCoverCapQuery);
+        return Ok(result);
+    }
+
+    [HttpPost("Add")]
+    public async Task<IActionResult> Add([FromBody] CreateCoverCapCommand createCoverCapCommand)
+    {
+        CreatedCoverCapResponse result = await _mediator.Send(createCoverCapCommand);
+        return Created(uri: "", result);
+    }
+
+    [HttpPut("Update")]
+    public async Task<IActionResult> Update([FromBody] UpdateCoverCapCommand updateCoverCapCommand)
+    {
+        UpdatedCoverCapResponse result = await _mediator.Send(updateCoverCapCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete("Delete")]
+    public async Task<IActionResult> Delete([FromBody] DeleteCoverCapCommand deleteCoverCapCommand)
+    {
+        DeletedCoverCapResponse result = await _mediator.Send(deleteCoverCapCommand);
+        return Ok(result);
     }
 }
