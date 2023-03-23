97c4a031be6209b346f7c5c87880004501bbfa4d
Furkan Bozkurt <77467154+FurkanHuman@users.noreply.github.com>
2023-03-24 00:47:11 +0300

    feat(Application): The services required for the country have been written.

--- Src/ProjectPackages/Application/Services/CountryService/CountryManager.cs
+++ Src/ProjectPackages/Application/Services/CountryService/CountryManager.cs
@@ -1,6 +1,28 @@
 // this file was created automatically.
+using Application.Features.Countries.Rules;
+using Application.Repositories;
+using Domain.Entities.Infos;
+
 namespace Application.Services.CountryService;
 
 public class CountryManager : ICountryService
 {
+    private readonly CountryBusinessRules _countryBusinessRules;
+    private readonly ICountryRepository _countryRepository;
+
+    public CountryManager(CountryBusinessRules countryBusinessRules, ICountryRepository countryRepository)
+    {
+        _countryBusinessRules = countryBusinessRules;
+        _countryRepository = countryRepository;
+    }
+
+    public Country GetCountry(int countryId)
+    {
+        return _countryRepository.GetAsync(c => c.Id == countryId).Result;
+    }
+
+    public void IdControlByCountry(Country country)
+    {
+        _countryBusinessRules.IdIsExit(country.Id);
+    }
 }
--- Src/ProjectPackages/Application/Services/CountryService/ICountryService.cs
+++ Src/ProjectPackages/Application/Services/CountryService/ICountryService.cs
@@ -1,6 +1,10 @@
 // this file was created automatically.
+using Domain.Entities.Infos;
+
 namespace Application.Services.CountryService;
 
 public interface ICountryService
 {
+    Country GetCountry(int countryId);
+    void IdControlByCountry(Country country);
 }
